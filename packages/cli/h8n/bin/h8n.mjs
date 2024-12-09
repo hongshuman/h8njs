d0f9b97f2adad73fad74f519e5e54fc792080181
Hong Shu-man <hongshuman@outlook.com>
2024-12-08 19:26:54 -0500

    feat: :tada: Hi~

--- packages/cli/h8n/bin/h8n.mjs
+++ packages/cli/h8n/bin/h8n.mjs
@@ -3,7 +3,7 @@
 import logger from '@h8n/logger';
 import utils from '@h8n/utils';
 
-console.log("Hi h8njs:cli/h8n - Hong Shu-man");
+console.log("Hi~ h8njs:cli/h8n - Hong Shu-man");
 
 logger.printMsg();
 utils.printMsg();

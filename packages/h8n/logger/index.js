313c9dd5e662f56a77095a7ac50bff108c8f38d4
Hong Shu-man <hongshuman@outlook.com>
2024-12-10 01:55:55 -0500

    feat: :tada: A Initial commit10.

--- packages/h8n/logger/index.js
+++ packages/h8n/logger/index.js
@@ -1,5 +1,5 @@
 console.log("@h8n/logger fix - Hong Shu-man");
 
 exports.printMsg = function() {
-  console.log("from the @h8n/logger package");
+  console.log("from kkkkk the @h8n/logger package");
 }
